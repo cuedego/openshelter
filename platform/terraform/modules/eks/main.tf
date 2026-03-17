c0a19b8bfab55b66c9fe84468b8823b87e54cf2e
cuedego <14223178-cuedego@users.noreply.gitlab.com>
2026-03-17 06:59:41 +0100

    fix(eks): enable API auth mode for access entries

--- platform/terraform/modules/eks/main.tf
+++ platform/terraform/modules/eks/main.tf
@@ -51,6 +51,11 @@ resource "aws_eks_cluster" "this" {
   name     = var.name
   role_arn = aws_iam_role.cluster.arn
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   vpc_config {
     subnet_ids         = var.subnet_ids
     security_group_ids = [aws_security_group.cluster.id]
